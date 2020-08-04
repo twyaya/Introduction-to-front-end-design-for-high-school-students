d77c70179ee4776c2b1965a16ef92aab79d96c9f
張皓偉 <otis8496@gmail.com>
2020-08-04 17:29:14 +0800

    整合

---
+++ hello-world/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <input type="text" v-model="myname" value="myname">
+    <p>大家好！我叫 {{ myname }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        myname:'請輸入姓名'
+      }
+    }
+  }
+</script>
+
+<style></style>
